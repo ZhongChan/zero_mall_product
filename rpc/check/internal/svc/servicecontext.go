f7397d2ed7b84b365b439e468c0ca10945256b73
SM1701 <717513736@qq.com>
2021-11-16 11:14:27 +0800

    check rpc

---
+++ rpc/check/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "zero-mall/zero_mall_product/rpc/check/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
